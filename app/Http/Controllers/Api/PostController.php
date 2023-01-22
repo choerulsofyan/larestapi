0c332dba313311bb91bef18f2cf94342f0ea694a
Choerul Sofyan <choerulsofyanmf@gmail.com>
2023-01-22 18:25:17 +0700

    create and fetch all posts data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -14,5 +15,29 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
 
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
